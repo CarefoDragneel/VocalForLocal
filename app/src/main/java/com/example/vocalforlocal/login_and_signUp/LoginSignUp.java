bbc2fc71cf1d0e51ba536cc7b660d09a12d7a8c0
hackeramitkumar <amit9116260192@gmail.com>
2022-04-23 02:29:10 +0530

    added navigation drawer and updated colors

--- app/src/main/java/com/example/vocalforlocal/login_and_signUp/LoginSignUp.java
+++ app/src/main/java/com/example/vocalforlocal/login_and_signUp/LoginSignUp.java
@@ -23,7 +23,6 @@ public class LoginSignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_sign_up);
-        getSupportActionBar().hide();
 
         tabLayout = findViewById(R.id.tab_layout);
         viewPager = findViewById(R.id.view_pager);
